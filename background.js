237cd065ec6678f55fd41234b38d500383867587
niko-u <77936525+niko-u@users.noreply.github.com>
2021-07-27 19:40:56 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,15 +1,48 @@
-console.log("Background running...");
 
-let text = "";
+function loadScript(link) {
+    var script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.src = link;
+    document.head.appendChild(script);
+}
+
+loadScript("https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder")
+
+console.log("Background running...");
+loadModel();
+
+window.text = "";
 window.rating;
 window.percentage;
 
 chrome.runtime.onMessage.addListener(receiver);
 
 function receiver (request, sender, sendResponse) {
+    if (request.start == true) {
+        start();
+    }
     console.log(request);
     text = request.text;
 }
 
+async function loadModel() {
+    console.log("Loading model...");
+    model = undefined;
+    model = await tf.loadLayersModel("https://raw.githubusercontent.com/niko-u/News-O-Meter/main/model.json");
+    console.log("Model Loaded");
+}
 
+function start() {
+    input = textPreprocessing(text);
+    //var output = model.predict(input);
+    console.log(input);
+}
 
+async function textPreprocessing(text) {
+    text = text.trim()
+    text = use.loadTokenizer().then(tokenizer => {
+        tokenizer.encode(text);
+      });
+    console.log(text);    
+    return text;
+}
